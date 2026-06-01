6ed25f841a236aa68de99798b80a4224127a093e
ke-Pratik <94440385+ke-Pratik@users.noreply.github.com>
2026-06-02 00:13:10 +0530

    Add LocalTime import for BulkPaymentRequest

--- backend/src/main/java/com/studycenter/dto/BulkPaymentRequest.java
+++ backend/src/main/java/com/studycenter/dto/BulkPaymentRequest.java
@@ -5,7 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+import java.time.LocalTime;
 import java.math.BigDecimal;
 import java.util.List;
 
